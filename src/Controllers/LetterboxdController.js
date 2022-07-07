e72f66250c4908650bf47be81f52f25fd0340e16
comicodarko <bitotajima@hotmail.com>
2022-07-07 10:18:53 -0300

    🩹 - Letterboxd Diary - Validação caso não encontre.

--- src/Controllers/LetterboxdController.js
+++ src/Controllers/LetterboxdController.js
@@ -4,22 +4,23 @@ const rss = new rssParser();
 module.exports = {
   async getDiary(req, res) {
     rss.parseURL(`https://letterboxd.com/${req.params.user}/rss`).then(response => {
-      let data = [];
+      let movies = [];
       response.items.forEach(item => {
-        const poster = 
-          item.content.slice(
-            item.content.indexOf('<img src="') + 10, 
-            item.content.indexOf('"/>'));
-        data.push({
+        const poster = item.content.slice(
+          item.content.indexOf('<img src="') + 10, 
+          item.content.indexOf('"/>'));
+        movies.push({
           title: item.title,
           review: item.contentSnippet,
           poster
         });
       });
-      res.json({ data });
+      res.json({ movies });
     }).catch(err => {
-      res.status(500);
-      res.json({ message: err });
+      err.message.includes('404')
+        ? res.status(404)
+        : res.status(500);
+      res.json({ message: err.message });
     });
 
   }
